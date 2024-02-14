66ba210031cceb87db083330b0422e39abdcf4bf
Alan <Alan@DESKTOP-TG930HR>
2024-02-14 13:29:02 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Form__ejercicio2_calcular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_valor1 = new System.Windows.Forms.Label();
+            this.txt_valor1 = new System.Windows.Forms.TextBox();
+            this.txt_valor2 = new System.Windows.Forms.TextBox();
+            this.lbl_valor2 = new System.Windows.Forms.Label();
+            this.txt_resta = new System.Windows.Forms.TextBox();
+            this.lbl_resta = new System.Windows.Forms.Label();
+            this.txt_multiplicacion = new System.Windows.Forms.TextBox();
+            this.lbl_Multiplicacion = new System.Windows.Forms.Label();
+            this.txt_divicion = new System.Windows.Forms.TextBox();
+            this.lbl_divicion = new System.Windows.Forms.Label();
+            this.txt_suma = new System.Windows.Forms.TextBox();
+            this.lbl_suma = new System.Windows.Forms.Label();
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Cerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_valor1
+            // 
+            this.lbl_valor1.AutoSize = true;
+            this.lbl_valor1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_valor1.Location = new System.Drawing.Point(252, 73);
+            this.lbl_valor1.Name = "lbl_valor1";
+            this.lbl_valor1.Size = new System.Drawing.Size(57, 20);
+            this.lbl_valor1.TabIndex = 0;
+            this.lbl_valor1.Text = "Valor1";
+            // 
+            // txt_valor1
+            // 
+            this.txt_valor1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_valor1.Location = new System.Drawing.Point(356, 67);
+            this.txt_valor1.Name = "txt_valor1";
+            this.txt_valor1.Size = new System.Drawing.Size(146, 28);
+            this.txt_valor1.TabIndex = 1;
+            // 
+            // txt_valor2
+            // 
+            this.txt_valor2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_valor2.Location = new System.Drawing.Point(356, 112);
+            this.txt_valor2.Name = "txt_valor2";
+            this.txt_valor2.Size = new System.Drawing.Size(146, 28);
+            this.txt_valor2.TabIndex = 3;
+            // 
+            // lbl_valor2
+            // 
+            this.lbl_valor2.AutoSize = true;
+            this.lbl_valor2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_valor2.Location = new System.Drawing.Point(252, 118);
+            this.lbl_valor2.Name = "lbl_valor2";
+            this.lbl_valor2.Size = new System.Drawing.Size(57, 20);
+            this.lbl_valor2.TabIndex = 2;
+            this.lbl_valor2.Text = "Valor2";
+            // 
+            // txt_resta
+            // 
+            this.txt_resta.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_resta.Location = new System.Drawing.Point(133, 228);
+            this.txt_resta.Name = "txt_resta";
+            this.txt_resta.Size = new System.Drawing.Size(100, 28);
+            this.txt_resta.TabIndex = 5;
+            // 
+            // lbl_resta
+            // 
+            this.lbl_resta.AutoSize = true;
+            this.lbl_resta.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_resta.Location = new System.Drawing.Point(158, 190);
+            this.lbl_resta.Name = "lbl_resta";
+            this.lbl_resta.Size = new System.Drawing.Size(49, 20);
+            this.lbl_resta.TabIndex = 4;
+            this.lbl_resta.Text = "Resta";
+            // 
+            // txt_multiplicacion
+            // 
+            this.txt_multiplicacion.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_multiplicacion.Location = new System.Drawing.Point(317, 228);
+            this.txt_multiplicacion.Name = "txt_multiplicacion";
+            this.txt_multiplicacion.Size = new System.Drawing.Size(100, 28);
+            this.txt_multiplicacion.TabIndex = 7;
+            // 
+            // lbl_Multiplicacion
+            // 
+            this.lbl_Multiplicacion.AutoSize = true;
+            this.lbl_Multiplicacion.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Multiplicacion.Location = new System.Drawing.Point(313, 190);
+            this.lbl_Multiplicacion.Name = "lbl_Multiplicacion";
+            this.lbl_Multiplicacion.Size = new System.Drawing.Size(118, 20);
+            this.lbl_Multiplicacion.TabIndex = 6;
+            this.lbl_Multiplicacion.Text = "Multiplicacion";
+            // 
+            // txt_divicion
+            // 
+            this.txt_divicion.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_divicion.Location = new System.Drawing.Point(469, 228);
+            this.txt_divicion.Name = "txt_divicion";
+            this.txt_divicion.Size = new System.Drawing.Size(100, 28);
+            this.txt_divicion.TabIndex = 9;
+            // 
+            // lbl_divicion
+            // 
+            this.lbl_divicion.AutoSize = true;
+            this.lbl_divicion.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_divicion.Location = new System.Drawing.Point(495, 190);
+            this.lbl_divicion.Name = "lbl_divicion";
+            this.lbl_divicion.Size = new System.Drawing.Size(74, 20);
+            this.lbl_divicion.TabIndex = 8;
+            this.lbl_divicion.Text = "Divicion";
+            // 
+            // txt_suma
+            // 
+            this.txt_suma.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_suma.Location = new System.Drawing.Point(641, 228);
+            this.txt_suma.Name = "txt_suma";
+            this.txt_suma.Size = new System.Drawing.Size(100, 28);
+            this.txt_suma.TabIndex = 11;
+            // 
+            // lbl_suma
+            // 
+            this.lbl_suma.AutoSize = true;
+            this.lbl_suma.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_suma.Location = new System.Drawing.Point(667, 190);
+            this.lbl_suma.Name = "lbl_suma";
+            this.lbl_suma.Size = new System.Drawing.Size(49, 20);
+            this.lbl_suma.TabIndex = 10;
+            this.lbl_suma.Text = "Suma";
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Calcular.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Calcular.Location = new System.Drawing.Point(150, 304);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(153, 51);
+            this.btn_Calcular.TabIndex = 12;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = false;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Limpiar.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(338, 304);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(153, 51);
+            this.btn_Limpiar.TabIndex = 13;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = false;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Cerrar
+            // 
+            this.btn_Cerrar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Cerrar.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cerrar.Location = new System.Drawing.Point(511, 304);
+            this.btn_Cerrar.Name = "btn_Cerrar";
+            this.btn_Cerrar.Size = new System.Drawing.Size(153, 51);
+            this.btn_Cerrar.TabIndex = 14;
+            this.btn_Cerrar.Text = "Cerrar";
+            this.btn_Cerrar.UseVisualStyleBackColor = false;
+            this.btn_Cerrar.Click += new System.EventHandler(this.btn_Cerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Cerrar);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.txt_suma);
+            this.Controls.Add(this.lbl_suma);
+            this.Controls.Add(this.txt_divicion);
+            this.Controls.Add(this.lbl_divicion);
+            this.Controls.Add(this.txt_multiplicacion);
+            this.Controls.Add(this.lbl_Multiplicacion);
+            this.Controls.Add(this.txt_resta);
+            this.Controls.Add(this.lbl_resta);
+            this.Controls.Add(this.txt_valor2);
+            this.Controls.Add(this.lbl_valor2);
+            this.Controls.Add(this.txt_valor1);
+            this.Controls.Add(this.lbl_valor1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_valor1;
+        private System.Windows.Forms.TextBox txt_valor1;
+        private System.Windows.Forms.TextBox txt_valor2;
+        private System.Windows.Forms.Label lbl_valor2;
+        private System.Windows.Forms.TextBox txt_resta;
+        private System.Windows.Forms.Label lbl_resta;
+        private System.Windows.Forms.TextBox txt_multiplicacion;
+        private System.Windows.Forms.Label lbl_Multiplicacion;
+        private System.Windows.Forms.TextBox txt_divicion;
+        private System.Windows.Forms.Label lbl_divicion;
+        private System.Windows.Forms.TextBox txt_suma;
+        private System.Windows.Forms.Label lbl_suma;
+        private System.Windows.Forms.Button btn_Calcular;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Cerrar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form__ejercicio2_calcular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Cerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            int Valor1 = Convert.ToInt32(txt_valor1.Text);
+            int Valor2 = Convert.ToInt32(txt_valor2.Text);
+
+
+
+            int Resta, Multiplicacion, Division, Suma;
+
+            Resta = Valor1 - Valor2;
+            Multiplicacion = Valor2 * Valor1;
+            Division = Valor1 / Valor2;
+            Suma = Valor2 + Valor1;
+
+
+
+            txt_resta.Text = Resta.ToString();
+
+            txt_multiplicacion.Text = Multiplicacion.ToString();
+
+            txt_divicion.Text = Division.ToString();
+
+            txt_suma.Text = Suma.ToString();
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_valor1.Clear();
+            txt_valor2.Clear();
+            txt_suma.Clear();
+            txt_resta.Clear();
+            txt_multiplicacion.Clear();
+            txt_divicion.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_suma.Enabled = false;
+            txt_resta.Enabled = false;
+            txt_multiplicacion.Enabled = false;
+            txt_divicion.Enabled = false;
+        }
+    }
+}
